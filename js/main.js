34985af116c5a71285a53029433448c55d667fc8
birchy009 <birchy009@hotmail.com>
2015-01-21 14:30:18 -0500

    feat(about): added content
    attempted to use “scrollTo”

--- js/main.js
+++ js/main.js
@@ -34,3 +34,14 @@
     });
 
 })( window );
+
+// credit to http://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2/ for smooth scroll
+$('a[href^="#"]').on('click', function(event) {
+    var target = $( $(this).attr('href') );
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+});
